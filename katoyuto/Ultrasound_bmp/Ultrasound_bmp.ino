96f6110ecf1356c4298eb9eec1fbc803b37d5280
DDD3H <40761694+DDD3H@users.noreply.github.com>
2022-02-15 12:03:22 +0900

    Create Ultrasound_bmp.ino

---
+++ katoyuto/Ultrasound_bmp/Ultrasound_bmp.ino
@@ -0,0 +1,19 @@
+long a;
+#include "SR04.h"
+#define TRIG_PIN 0
+#define ECHO_PIN 4
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float Temperature = 13.5; // BMPで取得した温度を入力
+  sound_velo = 331.5 + 0.6 * Temperature;
+  a = sr04.Distance()/340 * sound_velo;
+  Serial.println(a);
+}
